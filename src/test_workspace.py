587a1dbd57db4db169b759a13a6ec1ba3d17e991
davidchen <davidchen@gliacloud.com>
2017-11-05 21:19:30 +0800

    init

---
+++ src/test_workspace.py
@@ -0,0 +1,39 @@
+import os
+import urlparse
+
+import requests
+
+from workspace import cache, local, remote, tmp
+
+
+class TestWorkspace(object):
+    def test_tmp(self):
+        with tmp() as ofolder:
+            assert os.path.exists(ofolder)
+            assert os.path.isdir(ofolder)
+
+        assert not os.path.exists(ofolder)
+
+    def test_cache(self):
+        @cache(u'./tmp/{0}-{1}-{2}')
+        def _test(a, b, c, opath):
+            with open(opath, 'w') as ofile:
+                ofile.write('%s-%s-%s' % (a, b, c))
+
+            return opath
+
+        opath = _test('a', 'b', 'c')
+
+        assert os.path.basename(opath) == 'a-b-c'
+        assert os.path.exists(opath)
+        assert open(opath).read() == 'a-b-c'
+
+    def test_local(self):
+        ofile = local('http://via.placeholder.com/350x150.jpg')
+
+        assert os.path.basename(ofile) == '350x150.jpg'
+
+    def test_remote(self):
+        url = remote('./350x150.jpg')
+        resp = requests.get(url)
+        assert resp.status_code == 200
